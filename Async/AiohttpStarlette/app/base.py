a245933d903b3ed17e33419fe509d899ec22c3a7
maxik <kohiry@mail.ru>
2024-10-19 05:03:44 +0300

    [*] find issue in base with circular import

--- Async/AiohttpStarlette/app/base.py
+++ Async/AiohttpStarlette/app/base.py
@@ -3,9 +3,13 @@ from abc import ABCMeta, abstractmethod
 
 __all__ = ["BaseServer"]
 
+# from app.starlette_service.routers import BaseRouter
+# TODO тут проблема циркулярного импорта, так как ты не сделал базовую абстракцию роутинга выше starlette библотеки
+
 
 class BaseServer(metaclass=ABCMeta):
     __app: object
+    __routers: list
 
     @abstractmethod
     def _add_routes(self):
